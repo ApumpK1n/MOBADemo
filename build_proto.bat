fb172ea1755316e564f589a759e13af496fc8dd4
ApumpK1n <lsmzng@163.com>
2022-12-06 11:13:59 +0800

    Add test proto

---
+++ build_proto.bat
@@ -0,0 +1,16 @@
+:: 批量解析protocol文件
+
+@echo off
+
+set TOOl_PATH=./Protocol/tool/protoc-21.10-win64/bin/protoc.exe
+set IMPORT_PATH=./Protocol/proto/
+set DST_DIR=./Unity/Assets/Scripts/Proto/
+
+:: exe protoc
+
+for /f %%f in ('dir /b /s Protocol\proto\') do (
+    If not exist %%f echo.>%%f
+    "%TOOl_PATH%" --proto_path %IMPORT_PATH% --csharp_out %DST_DIR% %%~nxf
+     echo build %%f
+)
+pause
